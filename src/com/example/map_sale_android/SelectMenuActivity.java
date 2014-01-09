500ea0f53a479c237ba52298f85dde00736b8e19
nobezawa <nobesawa@nobezawa.local>
2014-01-09 22:55:12 +0900

    select menu activity

--- src/com/example/map_sale_android/SelectMenuActivity.java
+++ src/com/example/map_sale_android/SelectMenuActivity.java
@@ -1,15 +1,38 @@
 package com.example.map_sale_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class SelectMenuActivity extends Activity {
+public class SelectMenuActivity extends Activity implements OnClickListener{
 
-	/** Called when the activity is first created. */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_select_menu);
+	    
+	    Button now_where_button  = (Button) findViewById(R.id.now_where_button);
+	    Button serch_button  = (Button) findViewById(R.id.serch_button);
+	    now_where_button.setOnClickListener(this);
+	    serch_button.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id){
+		   case R.id.now_where_button:
+			   Intent intent = new Intent(this, NowWhereSearchActivity.class);
+			   startActivity(intent);
+		   case R.id.serch_button:
+			   Intent intent2 = new Intent(this, FormSearchActivity.class);
+			   startActivity(intent2);
+		}
+		
 	}
 
 }
