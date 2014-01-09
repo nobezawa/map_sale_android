7573504589c6d560c70ec57f354255e7e49c3734
nobezawa <nobesawa@nobezawa.local>
2014-01-09 22:14:05 +0900

    main activity layout

--- src/com/example/map_sale_android/MainActivity.java
+++ src/com/example/map_sale_android/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.map_sale_android;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,23 @@ public class MainActivity extends Activity {
 		   WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		   requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		LinearLayout all_screen = (LinearLayout) findViewById(R.id.all_screen);
+		all_screen.setOnClickListener(this);
+		
 	}
+	
+	@Override
+	public void onClick(View v){
+    	int id = v.getId();
+    	if (id == R.id.all_screen){
+    		Intent intent = new Intent(this, SelectMenuActivity.class);
+    		startActivity(intent);
+    		finish();
+    	}
+    	
+    }
+
+	
 
 
 }
