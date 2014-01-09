856cc63c69236eca0f3274227c7a9f8604d45e8a
のべざわ <nobezawa@54style.co.jp>
2014-01-09 18:17:57 +0900

    string edit

--- src/com/example/map_sale_android/MainActivity.java
+++ src/com/example/map_sale_android/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.map_sale_android;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().addFlags(
+		   WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		   requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 	}
 
