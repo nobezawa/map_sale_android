bad40dafb74625a02d503f490ad055b13e23be4d
のべざわ <nobezawa@54style.co.jp>
2014-01-13 22:45:13 +0900

    gps

---
+++ src/com/example/map_sale_android/FormSearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.map_sale_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FormSearchActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    
+        // TODO Auto-generated method stub
+    }
+
+}
